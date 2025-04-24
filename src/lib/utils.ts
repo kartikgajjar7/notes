5f6e18612473ab54c14a44c6d96995bfe845aec7
kartik suthar <kartiksuthar2756@gmail.com>
2025-04-24 07:56:10 +0530

    asd

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 import { redirect } from "next/navigation";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 export function encodedRedirect(
   type: "error" | "success",
   path: string,
-  message: string,
+  message: string
 ) {
-  return redirect(`${path}?${type}=${encodeURIComponent(message)}`)
+  return redirect(`${path}?${type}=${encodeURIComponent(message)}`);
+}
