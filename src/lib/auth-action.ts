6edc85c81de80701410e6b5de808c8ce453edfab
kartik suthar <kartiksuthar2756@gmail.com>
2025-04-24 09:01:16 +0530

    asd

--- src/lib/auth-action.ts
+++ src/lib/auth-action.ts
@@ -68,6 +68,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
+      redirectTo: "https://kartiknotes.vercel.app/dashboard",
       queryParams: {
         access_type: "offline",
         prompt: "consent",
