71bc893c139a4a7a0ebedc79768804dbb504c9b3
kartik suthar <kartiksuthar2756@gmail.com>
2025-04-24 08:49:11 +0530

    asd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "build",
   typescript: {
     // ⚠️ Dangerously allow production builds to successfully complete even if
     // your project has type errors.
